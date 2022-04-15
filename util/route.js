eaafb15e6e76371ce60b111a3fcc0a2a813c6a64
ywg <2521408103@qq.com>
2022-04-15 11:30:00 +0800

    init

--- util/route.js
+++ util/route.js
@@ -1,6 +1,6 @@
 class HashRouter {
   constructor() {
-    this.currentPath = '/';
+    this.currentPath = 'imageCut';
     this.routes = {}
   }
   init() {
@@ -8,16 +8,16 @@ class HashRouter {
     window.addEventListener('hashchange', this.updateView.bind(this))
   }
   updateView() {
-    this.currentPath = location.hash.substring(1) || '/'
+    this.currentPath = location.hash.substring(1) || 'imageCut'
     this.routes[this.currentPath] && this.routes[this.currentPath]()
+    // console.log(this.currentPath,this.routes[this.currentPath],this.routes)
   }
   route(path, callback) {
     this.routes[path] = callback
   }
 }
-
 const router = new HashRouter();
-router.init();
+
 [].forEach.call(myTab.children, function (item) {
   const i = item.children[0].href.split('#')[1];
   const d = document.getElementById(`${i}`);
@@ -25,3 +25,6 @@ router.init();
     myTabContent.innerHTML = d.innerHTML;
   })
 })
+
+
+router.init();
